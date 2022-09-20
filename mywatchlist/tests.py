b6bfa8590591339ec0d87904858df3797ef7a56f
Alanna <alanna@alanna.local>
2022-09-20 22:02:34 +0700

    Updated Procfile

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -4,8 +4,7 @@ from django.urls import reverse
 class TestViews(TestCase):
 
     def test_show_html_resolved(self):  
-        client = Client()       
-        response = client.get(reverse("mywatchlist:show_mywatchlist"))
+        response = Client() .get(reverse("mywatchlist:show_mywatchlist"))
         self.assertEqual(response.status_code, 200)
     
     def test_show_xml_resolved(self):         
